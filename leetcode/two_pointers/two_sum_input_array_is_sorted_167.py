7002963184fccfe63bebc056c4e45ea4302f34ca
Yevgeniy Putrin <yevgeniyputrin@Yevgeniys-MacBook-Air.local>
2024-08-31 23:48:46 +0300

    two pointers solution

--- leetcode/two_pointers/two_sum_input_array_is_sorted_167.py
+++ leetcode/two_pointers/two_sum_input_array_is_sorted_167.py
@@ -1,17 +1,20 @@
 from typing import List
 
-
-# [2,3,4], 6
+# [5,25,75], 100
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        idx = 1
-        hash_map = {numbers[0] : 0}
-        while idx < len(numbers):
-            diff = target - numbers[idx]
-            if diff in hash_map:
-                return [hash_map[diff] + 1, idx + 1]
-            hash_map[numbers[idx]] = idx
-            idx += 1
+        l = 0
+        r = len(numbers) - 1
+        while True:
+            summ = numbers[l] + numbers[r]
+            if summ == target:
+                return [l + 1, r + 1]
+            if summ > target:
+                r -= 1
+            else:
+                l += 1
 
 
-
+s = Solution()
+result = s.twoSum([3,24,50,79,88,150,345], 200)
+print(result)
