a401e1f3f54bf4ec19cb3fed54c520686fc302e0
Yevgeniy Putrin <yevgeniyputrin@Yevgeniys-MacBook-Air.local>
2024-09-01 16:03:26 +0300

    optimized bin search

--- leetcode/binary_search/binary_search.py
+++ leetcode/binary_search/binary_search.py
@@ -21,6 +21,23 @@ class Solution:
                 end = mid
                 mid = end // 2
         return -1
+# nums = [-1,0,3,5,9,12], target = 5
+    def search_optimized(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            mid_value = nums[mid]
+
+            if mid_value == target:
+                return mid
+            elif mid_value < target:
+                start = mid + 1
+            else:  # mid_value > target
+                end = mid - 1
+
+        return -1
 
 s = Solution()
 print(s.search(nums = [-1,0,3,5,9,12], target = 3))
