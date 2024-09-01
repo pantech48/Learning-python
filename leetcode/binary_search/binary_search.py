8dc4c87af4677a7e415e2554aa7c063e5c4ac242
Yevgeniy Putrin <yevgeniyputrin@Yevgeniys-MacBook-Air.local>
2024-09-01 15:53:18 +0300

    not optimized bin search alg

---
+++ leetcode/binary_search/binary_search.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+# nums = [-1,0,3,5,9,12], target = 9
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        mid = len(nums) // 2
+        end = len(nums)
+        prev_mid = -1
+        while prev_mid != mid:
+            mid_value = nums[mid]
+            if mid_value == target:
+                return mid
+            elif mid_value < target:
+                start = mid
+                prev_mid = mid
+                mid = (start + end) // 2
+            elif mid_value > target:
+                prev_mid = mid
+                end = mid
+                mid = end // 2
+        return -1
+
+s = Solution()
+print(s.search(nums = [-1,0,3,5,9,12], target = 3))
